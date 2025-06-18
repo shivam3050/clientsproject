777fa98bd592960bc336ebf1a4eeb192bc589b12
shivam3050 <shivamkumar30508209@gmail.com>
2025-06-18 16:08:18 +0530

    some bugs in build fixed

--- backend/server.js
+++ backend/server.js
@@ -386,7 +386,6 @@ connectDB()
                 sameSite: "Lax",
                 path: "/"  ,                  
                 maxAge: 7 * 24 * 60 * 60 * 1000
-
             }
             return res
                 .status(200)

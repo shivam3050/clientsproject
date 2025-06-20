1fd5434390c31ac4f08a013fbec4b51cdc6f5858
shivam3050 <shivamkumar30508209@gmail.com>
2025-06-20 15:40:00 +0530

    s

--- frontend/admin/src/pages/AdminSubjectDetail.jsx
+++ frontend/admin/src/pages/AdminSubjectDetail.jsx
@@ -135,7 +135,7 @@ export const AdminSubjectDetail = () => {
                                 <div id="iframeContainer" >
                                     <iframe src={`https://drive.google.com/file/d/${item.fileid}/preview`}
                                         allow="fullscreen"
-                                        
+                                        height="200px" width="200px"
                                         
                                         frameBorder="0" ></iframe>
                                 </div>
